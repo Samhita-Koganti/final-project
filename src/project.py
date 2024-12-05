c8b8e200b69fda9b5b55a3b3244ab5b88df61e02
Samhita Koganti <koganti.samhita@gmail.com>
2024-12-04 20:28:46 -0600

    Created basic game loop.

--- src/project.py
+++ src/project.py
@@ -8,3 +8,20 @@ window_width = 600
 window_height = 600
 game_window = pygame.display.set_mode((window_width, window_height))
 pygame.display.set_caption("Python Snake Game")
+
+# create game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    # update game status
+
+    # render graphics
+
+    pygame.display.update()
+
+# quit game and clean up resources
+pygame.quit()
+sys.exit()
