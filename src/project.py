99e3a784bf5f78e4fc5e0cfc1a90e1326ee41b78
Samhita Koganti <koganti.samhita@gmail.com>
2024-12-04 22:21:29 -0600

    Added fruit and snake.

--- src/project.py
+++ src/project.py
@@ -1,10 +1,41 @@
 import pygame
 import sys
+import random
+from pygame.math import Vector2
+
+# create snake function
+class SNAKE:
+    def __init__(self):
+        self.body = [Vector2(5,10),Vector2(6,10),Vector2(7,10)]
+
+    def draw_snake(self):
+        for block in self.body:
+            x_pos = int(block.x * cell_size)
+            y_pos = int(block.y * cell_size)
+            block_rect = pygame.Rect(x_pos,y_pos,cell_size,cell_size)
+            pygame.draw.rect(screen,(183,111,122),block_rect)
+
+# create fruit function
+class FRUIT:
+    def __init__(self):
+        self.x = random.randint(0,cell_number - 1)
+        self.y = random.randint(0,cell_number - 1)
+        self.pos = Vector2(self.x,self.y)
+
+    def draw_fruit(self):
+        fruit_rect = pygame.Rect(int(self.pos.x * cell_size), int(self.pos.y * cell_size),cell_size,cell_size)
+        pygame.draw.rect(screen,(126,166,114),fruit_rect) # red
 
 # create game window
 pygame.init()
-screen = pygame.dusplay.set_mode((600, 600))
+cell_size = 40
+cell_number = 20
+screen = pygame.display.set_mode((cell_number * cell_size,cell_number * cell_size))
 clock = pygame.time.Clock()
+
+# define variables
+fruit = FRUIT()
+snake = SNAKE()
 
 # create game loop
 while True:
@@ -14,5 +45,7 @@ while True:
             sys.exit()
     
     screen.fill((175,215,70)) # green
+    fruit.draw_fruit()
+    snake.draw_snake()
     pygame.display.update()
     clock.tick(60)
