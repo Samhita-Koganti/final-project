22a6b16cb6007c9f3fcda688417f7e22cca5312d
Samhita Koganti <koganti.samhita@gmail.com>
2024-12-04 23:29:40 -0600

    Added "Score:" to score display.

--- src/project.py
+++ src/project.py
@@ -106,7 +106,7 @@ class MAIN:
 
     def draw_score(self):
         score_text = str(len(self.snake.body) -3)
-        score_surface = game_font.render(score_text,True,(56,74,12))
+        score_surface = game_font.render("Score: " + score_text,True,(56,74,12))
         score_x = int(cell_size * cell_number - 60)
         score_y = int(cell_size * cell_number - 40)
         score_rect = score_surface.get_rect(center = (score_x,score_y))
