0aaa0dbd9e0a619220cb30d8838c122dc701620c
Samhita Koganti <koganti.samhita@gmail.com>
2024-12-04 23:18:14 -0600

    Added score counter.

--- src/project.py
+++ src/project.py
@@ -60,6 +60,7 @@ class MAIN:
         self.draw_grass()
         self.fruit.draw_fruit()
         self.snake.draw_snake()
+        self.draw_score()
 
     def check_collision(self):
         if self.fruit.pos == self.snake.body[0]:
@@ -96,12 +97,21 @@ class MAIN:
                         grass_rect = pygame.Rect(col * cell_size,row * cell_size,cell_size,cell_size)
                         pygame.draw.rect(screen,grass_color,grass_rect)
 
+    def draw_score(self):
+        score_text = str(len(self.snake.body) -3)
+        score_surface = game_font.render(score_text,True,(56,74,12))
+        score_x = int(cell_size * cell_number - 60)
+        score_y = int(cell_size * cell_number - 40)
+        score_rect = score_surface.get_rect(center = (score_x,score_y))
+        screen.blit(score_surface,score_rect)
+
 # create game window
 pygame.init()
 cell_size = 40
 cell_number = 20
 screen = pygame.display.set_mode((cell_number * cell_size,cell_number * cell_size))
 clock = pygame.time.Clock()
+game_font = pygame.font.Font(None, 25)
 
 # refresh screen
 SCREEN_UPDATE = pygame.USEREVENT
